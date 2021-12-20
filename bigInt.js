64dc94eb2e61677c3c548c9e4da070ae1815603c
nibedita260 <deepi.nibedita260@gmail.com>
2021-12-20 11:09:44 +0530

    [Nibedita]Added:bigINT in Javascript Features Implemented

---
+++ bigInt.js
@@ -0,0 +1,4 @@
+//The biggest integer "usual" number type can handle is equal to 2 ** 53 - 1 or 9007199254740991. You can access this value under the MAX_SAFE_INTEGER constant.
+
+const x=Number.MAX_SAFE_INTEGER; // 9007199254740991
+console.log(x)
